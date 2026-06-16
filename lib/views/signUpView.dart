f50832c8f6d3b0f1a79c8718b0f325aeeb96fd60
Beaton505 <bheat12344@gmail.com>
2026-06-16 16:25:12 +0100

    Fixed email verification

--- lib/views/signUpView.dart
+++ lib/views/signUpView.dart
@@ -46,8 +46,8 @@ class Signupview extends StatefulWidget{
             const Text("Sign Up", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w700),),
             /* Email */
             const SizedBox(height: 50,),
-            TextField(controller: _email,
-            decoration: InputDecoration(labelText: "Email",errorText: _validateEmail? "Value Can't Be Empty" : null,),style: TextStyle(fontSize: 20),),
+            TextFormField(controller: _email, validator: validateEmail,
+            decoration: InputDecoration(labelText: "Email",errorText: _validateEmail? "Value Can't Be Empty" : null),style: TextStyle(fontSize: 20),),
             
             /* Password */
             const SizedBox(height: 50,),
@@ -90,7 +90,7 @@ class Signupview extends StatefulWidget{
             
             /* lang */
             const SizedBox(height: 50,),
-            StreamBuilder(stream: signUpController().fetchLanguages(), builder: (context, snapshot){/* Stream builder  */
+            StreamBuilder(stream: signUpController().fetchLanguages(), builder: (context, snapshot){/* Stream builder */
               if (snapshot.hasData){
                 languages = snapshot.data!;
                 return DropdownMenu(textStyle: TextStyle(fontSize: 20), 
@@ -176,5 +176,20 @@ class Signupview extends StatefulWidget{
     }
     catch(e){}
   }
-  
+
+String? validateEmail(String? value) {
+  const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
+      r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
+      r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
+      r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
+      r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
+      r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
+      r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
+  final regex = RegExp(pattern);
+
+  return value!.isNotEmpty && !regex.hasMatch(value)
+      ? 'Enter a valid email address'
+      : null;
+}
+
 }
